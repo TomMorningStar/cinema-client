58ff8d791bab29b680b96f68bb82b25adc666567
Magomed Kuriev <magomedkuriev2001@gmail.com>
2022-03-22 11:46:01 +0300

    сделал раздел контакты

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import Contacts from '../contactsM/Contacts';
+
+
+
 
 const Main = () => {
     return (
         <div>
-            <Contacts/>
+            
+            
         </div>
     );
 };
